f6b4c6657310e8c7a328be604a526bf75132f5b9
Dmitriy Strok <all1son4>
2025-06-20 21:45:46 +0200

    Add cloudinary player

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,5 +27,21 @@ export default function Home() {
     fetchMedia();
   }, []);
 
-  return <MediaGrid content={media} />;
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Видео из Google Диска</h1>
+      {media.length > 0 ? (
+        <MediaGrid content={media} />
+      ) : (
+        <p>Загрузка видео...</p>
+      )}
+      <video controls className="w-full max-w-xs rounded-2xl shadow-lg">
+        <source
+          src="https://res.cloudinary.com/daeqbpscy/video/upload/v1750409980/dsonugcqfzeqe0d51aee.mov"
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
 }
