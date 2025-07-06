60e2f65328fadb8d0e11681c6c85726b3850706f
Dmitriy Strok <all1son4>
2025-07-07 00:51:00 +0200

    Fix scroll

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -132,7 +132,7 @@ export const MainPage = () => {
       {/* Contact Section */}
       <footer id="contact" className="bg-black text-white">
         <ComponentWrapper>
-          <section className="pt-12 pb-16 md:pt-24 pb-24 flex flex-col md:flex-row gap-12 md:gap-24">
+          <section className="pt-12 pb-16 md:pt-24 pb-24 flex flex-col md:flex-row gap-12 md:gap-24 w-full">
             <div className="flex-1 flex flex-col gap-6">
               <h2 className="text-3xl md:text-4xl font-bold">
                 Let’s create something beautiful together
